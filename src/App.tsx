4f48c5d2270ae77037bbb322648a44d11af1763a
Conway Hogan <conway2019hogan@gmail.com>
2024-03-08 16:03:33 -0700

    my changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
+import { Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const teams = teamsData.teams;
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Welcome to the list of college teams</h1>
+      <p>
+        This website just lists college teams. It does nothing else. It might as
+        well be a word doc.
+      </p>
+    </div>
+  );
+}
+class TeamCard extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div className="card">
+        <p className="team-school">{oneTeam.school}</p>
+        <p className="team-name">{oneTeam.name}</p>
+        <p className="team-location">
+          {oneTeam.city}, {oneTeam.state}
+        </p>
+      </div>
+    );
+  }
+}
+function TeamsList() {
+  return (
+    <Row>
+      {teams.map((team, index) => (
+        <Col key={index} xs={12} sm={6} md={4} lg={4}>
+          <TeamCard {...team} />
+        </Col>
+      ))}
+    </Row>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TeamsList />
     </div>
   );
 }
